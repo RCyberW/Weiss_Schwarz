46cb43ad3034d70fe7386e26ddc280f36472763c
Frank Chen <jia.chn@gmail.com>
2013-04-10 22:35:58 -0700

    Testing network communication

--- src/Connection/SocketClient.java
+++ src/Connection/SocketClient.java
@@ -1,20 +1,21 @@
 package Connection;
+
 import java.awt.Color;
 import java.awt.BorderLayout;
 import java.awt.event.*;
+
 import javax.swing.*;
 
 import java.io.*;
 import java.net.*;
 
-class SocketClient extends JFrame implements ActionListener {
+class SocketClient extends JFrame implements MouseListener {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 	JLabel text, clicked;
-	JButton button;
 	JPanel panel;
 	JTextField textField;
 	Socket socket = null;
@@ -24,44 +25,21 @@ class SocketClient extends JFrame implements ActionListener {
 	SocketClient() { // Begin Constructor
 		text = new JLabel("Text to send over socket:");
 		textField = new JTextField(20);
-		button = new JButton("Click Me");
-		button.addActionListener(this);
-
+		textField.addMouseListener(this);
 		panel = new JPanel();
 		panel.setLayout(new BorderLayout());
 		panel.setBackground(Color.white);
 		getContentPane().add(panel);
 		panel.add("North", text);
 		panel.add("Center", textField);
-		panel.add("South", button);
 	} // End Constructor
-
-	public void actionPerformed(ActionEvent event) {
-		Object source = event.getSource();
-
-		if (source == button) {
-			// Send data over socket
-			String text = textField.getText();
-			out.println(text);
-			textField.setText(new String(""));
-			// Receive text from server
-			try {
-				String line = in.readLine();
-				System.out.println("Text received :" + line);
-			} catch (IOException e) {
-				System.out.println("Read failed");
-				System.exit(1);
-			}
-		}
-	}
 
 	public void listenSocket() {
 		// Create socket connection
 		try {
-			socket = new Socket("Blade", 9090);
+			socket = new Socket("Frank-PC", 9090);
 			out = new PrintWriter(socket.getOutputStream(), true);
-			in = new BufferedReader(new InputStreamReader(
-					socket.getInputStream()));
+			in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 			out.println("hello world");
 		} catch (UnknownHostException e) {
 			System.out.println("Unknown host: Blade");
@@ -85,5 +63,91 @@ class SocketClient extends JFrame implements ActionListener {
 		frame.pack();
 		frame.setVisible(true);
 		frame.listenSocket();
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent arg0) {
+		// Send data over socket
+		String text = textField.getText();
+		text = "mouseClicked (" + arg0.getX() + ", " + arg0.getY() + ")";
+		out.println(text);
+		textField.setText(new String(""));
+		// Receive text from server
+		try {
+			String line = in.readLine();
+			System.out.println("Text received :" + line);
+		} catch (IOException e) {
+			System.out.println("Read failed");
+			System.exit(1);
+		}
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// Send data over socket
+		String text = textField.getText();
+		text = "mouseEntered (" + arg0.getX() + ", " + arg0.getY() + ")";
+		out.println(text);
+		textField.setText(new String(""));
+		// Receive text from server
+		try {
+			String line = in.readLine();
+			System.out.println("Text received :" + line);
+		} catch (IOException e) {
+			System.out.println("Read failed");
+			System.exit(1);
+		}
+
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// Send data over socket
+		String text = textField.getText();
+		text = "mouseExited (" + arg0.getX() + ", " + arg0.getY() + ")";
+		out.println(text);
+		textField.setText(new String(""));
+		// Receive text from server
+		try {
+			String line = in.readLine();
+			System.out.println("Text received :" + line);
+		} catch (IOException e) {
+			System.out.println("Read failed");
+			System.exit(1);
+		}
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		// Send data over socket
+		String text = textField.getText();
+		text = "mousePressed (" + arg0.getX() + ", " + arg0.getY() + ")";
+		out.println(text);
+		textField.setText(new String(""));
+		// Receive text from server
+		try {
+			String line = in.readLine();
+			System.out.println("Text received :" + line);
+		} catch (IOException e) {
+			System.out.println("Read failed");
+			System.exit(1);
+		}
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// Send data over socket
+		String text = textField.getText();
+		text = "mouseReleased (" + arg0.getX() + ", " + arg0.getY() + ")";
+		out.println(text);
+		textField.setText(new String(""));
+		// Receive text from server
+		try {
+			String line = in.readLine();
+			System.out.println("Text received :" + line);
+		} catch (IOException e) {
+			System.out.println("Read failed");
+			System.exit(1);
+		}
 	}
 }
